aa11367bf4337f89e32fa7538e53f5f7c840b030
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-24 19:31:14 +0100

    Plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+// Plan
+// Need to parse "3 m/s" to (3.0, ['m'], ["s"])
+// Start with floats, then maybe polymorphic
+// Then need routines for +-/*
