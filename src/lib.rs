ebc9486eb5bce0e68608cbdb182cd32cb3698e11
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-24 22:38:39 +0100

    Change to lib structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-}
 // Plan
 // Need to parse "3 m/s" to (3.0, ['m'], ["s"])
 // Start with floats, then maybe polymorphic
 // Then need routines for +-/*
+
