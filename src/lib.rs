4fa68d4b0de86fe6cbd8b0d2e9f0277508dec94a
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-24 22:40:53 +0100

    Copy in example of using Add trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,23 @@
 // Need to parse "3 m/s" to (3.0, ['m'], ["s"])
 // Start with floats, then maybe polymorphic
 // Then need routines for +-/*
+use std::ops::Add;
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -10,7 +26,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(
+            Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+            Point { x: 3, y: 3 }
+        );
     }
 }
