6b1e83f04fd522e715093b24738a238697253ac7
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-09-25 20:59:35 +0100

    Instead use a vector

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@
 // Start with floats, then maybe polymorphic
 // Then need routines for +-/*
 use std::ops::Add;
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
+#[derive(Debug, Clone, PartialEq)]
+struct Unit {
+    x: f64,
+    upper: Vec<String>
 }
 
-impl Add for Point {
+impl Add for Unit {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
         Self {
             x: self.x + other.x,
-            y: self.y + other.y,
+            upper: vec![]
         }
     }
 }
@@ -27,8 +27,8 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(
-            Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-            Point { x: 3, y: 3 }
+            Unit { x: 1., upper: vec![] } + Unit { x: 2., upper: vec![] },
+            Unit { x: 3., upper: vec![] }
         );
     }
 }
