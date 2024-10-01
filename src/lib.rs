0bb6f8246f2e22bb1530beddb487048a22e64916
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-01 21:33:45 +0100

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -2,44 +2,41 @@
 // Need to parse "3 m/s" to (3.0, ['m'], ["s"])
 // Start with floats, then maybe polymorphic
 // Then need routines for +-/*
-use std::ops::Add;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::ops::Add;
 #[derive(Debug, Clone, PartialEq)]
 struct Unit {
     x: f64,
-    units: HashMap<String,i64>
+    units: HashMap<String, i64>,
 }
 
 impl Add for Unit {
-    type Output = Result<Self,String>;
+    type Output = Result<Self, String>;
 
-    fn add(self, other: Self) -> Result<Self,String> {
+    fn add(self, other: Self) -> Result<Self, String> {
         if self.units == other.units {
             Ok(Self {
                 x: self.x + other.x,
                 units: self.units,
             })
-        }
-        else {
+        } else {
             Err("Units don't match".to_string())
         }
     }
 }
 
-fn combine(units1: HashMap<String,i64>, units2: HashMap<String,i64>) -> HashMap<String,i64> {
+fn combine(units1: HashMap<String, i64>, units2: HashMap<String, i64>) -> HashMap<String, i64> {
     let keys1 = units1.keys().collect::<HashSet<&String>>();
     let keys2 = units2.keys().collect::<HashSet<&String>>();
     let all_keys = keys1.union(&keys2);
     let mut result = HashMap::new();
     for key in all_keys {
-        let new_value = units1.get(&key.to_string()).unwrap_or(&0) + units2.get(&key.to_string()).unwrap_or(&0);
-        result.insert(key.to_string(),new_value);
+        let new_value =
+            units1.get(&key.to_string()).unwrap_or(&0) + units2.get(&key.to_string()).unwrap_or(&0);
+        result.insert(key.to_string(), new_value);
     }
     result
-
-
-
 }
 
 // How to combine two units
@@ -58,27 +55,28 @@ mod tests {
     #[test]
     fn basic_add() {
         assert_eq!(
-            Unit { x: 1., units: HashMap::from([("m".to_string(),1)]) } + Unit { x: 2., units: HashMap::from([("m".to_string(),1)]) },
-            Ok(Unit { x: 3., units: HashMap::from([("m".to_string(),1)])})
+            Unit {
+                x: 1.,
+                units: HashMap::from([("m".to_string(), 1)])
+            } + Unit {
+                x: 2.,
+                units: HashMap::from([("m".to_string(), 1)])
+            },
+            Ok(Unit {
+                x: 3.,
+                units: HashMap::from([("m".to_string(), 1)])
+            })
         );
     }
 
     #[test]
     fn combine_works() {
         assert_eq!(
-combine(
-    HashMap::from([
-    ("m".to_string(), 2),
-    ("s".to_string(), -2),
-]),
-    HashMap::from([
-    ("m".to_string(), 1),
-    ("s".to_string(), 1),
-])),
-    HashMap::from([
-    ("m".to_string(), 3),
-    ("s".to_string(), -1),
-]))
-
+            combine(
+                HashMap::from([("m".to_string(), 2), ("s".to_string(), -2),]),
+                HashMap::from([("m".to_string(), 1), ("s".to_string(), 1),])
+            ),
+            HashMap::from([("m".to_string(), 3), ("s".to_string(), -1),])
+        )
     }
 }
